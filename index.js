21a3c86770678e444f9ec59bdc94c378700c5ba1
MinhwanO <107867881+MinhwanO@users.noreply.github.com>
2023-01-14 20:22:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const port = 5000
 const { Post } = require('./post')
 const cors = require('cors')
 
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-}))
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
